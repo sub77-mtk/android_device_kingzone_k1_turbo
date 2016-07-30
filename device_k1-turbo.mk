38c93e32c7dc300b4340ad2588318b87acec25b9
fire855 <thefire855@gmail.com>
2016-07-30 17:58:13 +0200

    Add media profiles

--- device_k1-turbo.mk
+++ device_k1-turbo.mk
@@ -31,6 +31,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Wifi
